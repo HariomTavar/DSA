e126df207c2bfb63651472e03c0a8b824f729dcc
HariomTavar <hariomtavar7@gmail.com>
2026-08-20 22:52:00 +0530

    in python list and tuple done

---
+++ Python/pra_list.py
@@ -0,0 +1,4 @@
+#  list explanation
+
+friends = ["hari", "ram" ,56,5.5 ,False]
+print(friends[3])
---
+++ Python/pra_tuple.py
@@ -0,0 +1,14 @@
+#  explation of tupl
+
+
+a = (1,2,34,55.5,"rohan","shivam")
+
+# methods
+# count(value): how muuch have 
+# /
+
+# list enter some names with the help of user
+
+name =[]
+f1 = input("1st :")
+name.append(f1)
