ab044adcf975d60ee6e8a56afd96a3c1a8c4ec81
HariomTavar <hariomtavar7@gmail.com>
2026-08-27 22:30:35 +0530

    learn class and obj in python

---
+++ Python/Project_waterGunGame/Instnce.py
@@ -0,0 +1,13 @@
+class shyam:
+
+    lang = "Eng"
+    class_no = 12
+    
+    def _init_(self):
+        print("i am creating the obj :")
+
+
+hrs = shyam()
+
+
+
--- Python/Project_waterGunGame/Oops.py
+++ Python/Project_waterGunGame/Oops.py
@@ -1,1 +1,7 @@
 print("run code with the help of Object oreiented programming")
+class Emp :
+    def display():
+        print("its Emp class run ")
+
+hr = Emp  # here hr is object
+hr.display()
