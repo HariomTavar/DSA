14f320575f9e5c32c8e41547b81cc70f8e2cb554
HariomTavar <hariomtavar7@gmail.com>
2026-08-29 13:49:16 +0530

    Quick understanding

---
+++ Python/Project_waterGunGame/Quick_refernce.py
@@ -0,0 +1,31 @@
+print()       # Output
+
+input()       # Take Input
+
+int()         # Convert to Integer
+
+float()       # Convert to Float
+
+str()         # Convert to String
+
+len()         # Length
+
+range()       # Generate Numbers
+
+def           # Function
+
+return        # Return Value
+
+class         # Create Class
+
+self          # Current Object
+
+__init__      # Constructor
+
+try           # Handle Errors
+
+except        # Catch Errors
+
+open()        # Open File
+
+append()      # Add Element to List
